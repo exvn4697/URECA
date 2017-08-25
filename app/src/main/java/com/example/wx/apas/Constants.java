97904cad2dc7cb1ee0f3fbe03165ccfad8c9afd4
exvn4697 <ervin_4697@yahoo.com>
2017-08-25 23:45:28 +0800

    final deliverables fixed

--- app/src/main/java/com/example/wx/apas/Constants.java
+++ app/src/main/java/com/example/wx/apas/Constants.java
@@ -5,6 +5,6 @@ package com.example.wx.apas;
  */
 
 public final class Constants {
-    public static final String ROOT_URL = "http://192.168.0.103:8000";
+    public static final String ROOT_URL = "http://10.27.211.189:8000";
     public static final String SEPARATOR = "b18f33f8-0d0b-4a86-ae60-8c802e645bfc"; //same with question-panels.js
 }
